bf7a1cc5ac4b1a4b9e3d92463a85131a1cd6698f
tommaso <tommasozugno@gmail.com>
2019-02-18 18:36:08 +0100

    Update install.sh to read cmd line arguments

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
+# USAGE 1) Run "bash install.sh [scenario]". Available scenarios: 
+# (i) chemical-plant-scenario, (ii) mva-scenario,
+# (iii) high-school-shooting-scenario
+
+# USAGE 2) Run "bash install.sh" and follow the instructions
+
 NS3_MMWAVE_URL="https://github.com/nyuwireless-unipd/ns3-mmwave.git"
 NS3_MMWAVE_IAB_URL="https://github.com/signetlabdei/ns3-mmwave-iab"
 
-echo -e "This script will install the desired PSC scenario.\nChoose between the following:\n[1] Chemical plant explosion\n[2] Multi-vehicle accident\n[3] High school shooting\n"
-exit=false
-while [ $exit = false ]; do
-  read choice
-  if [ $choice = 1 ] || [ $choice = 2 ] || [ $choice = 3 ]; then
-    exit=true
+if [ $# -gt 0 ]; then
+  if [ "$1" == "chemical-plant-scenario" ]; then
+    choice=1
+  elif [ "$1" == "mva-scenario" ]; then
+    choice=2
+  elif [ "$1" == "high-school-shooting-scenario" ]; then
+    choice=3
   else
-    echo -e "Please choose 1, 2 or 3"
+    echo -e "Run "bash install.sh" [scenario]\n\nAvailable scenarios:\nchemical-plant-scenario\nmva-scenario\nhigh-school-shooting-scenario\n"
+    exit 1
   fi
-done
+else
+  echo -e "This script will install the desired PSC scenario.\nChoose between the following:\n[1] Chemical plant explosion\n[2] Multi-vehicle accident\n[3] High school shooting\n"
+  exit=false
+  while [ $exit = false ]; do
+    read choice
+    if [ $choice = 1 ] || [ $choice = 2 ] || [ $choice = 3 ]; then
+      exit=true
+    else
+      echo -e "Please choose 1, 2 or 3"
+    fi
+  done
+fi
 
 if [ $choice = 1 ]; then
   echo -e "Installing the chemical plant scenario..."
